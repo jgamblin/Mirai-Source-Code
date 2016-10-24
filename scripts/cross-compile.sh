a31da90559f51659c3491d7ceb94e3c573e00dfa
Placidina <alanplacidina@gmail.com>
2016-10-24 20:34:39 -0200

    Fix download bin cross-compiler

--- scripts/cross-compile.sh
+++ scripts/cross-compile.sh
@@ -20,30 +20,29 @@ apt-get install -y gcc golang electric-fence
 
 echo "Creating folder /etc/xcompile"
 mkdir /etc/xcompile > /dev/null 2>&1
-
-cd ../cross-compile-bin
-echo "Copy cross-compiler-armv4l.tar.bz2 to /etc/xcompile"
-cp cross-compiler-armv4l.tar.bz2 /etc/xcompile/cross-compiler-armv4l.tar.bz2
-echo "Copy cross-compiler-armv5l.tar.bz2 to /etc/xcompile"
-cp cross-compiler-armv5l.tar.bz2 /etc/xcompile/cross-compiler-armv5l.tar.bz2
-echo "Copy cross-compiler-armv6l.tar.bz2 to /etc/xcompile"
-cp cross-compiler-armv6l.tar.bz2 /etc/xcompile/cross-compiler-armv6l.tar.bz2
-echo "Copy cross-compiler-i586.tar.bz2 to /etc/xcompile"
-cp cross-compiler-i586.tar.bz2 /etc/xcompile/cross-compiler-i586.tar.bz2
-echo "Copy cross-compiler-m68k.tar.bz2 to /etc/xcompile"
-cp cross-compiler-m68k.tar.bz2 /etc/xcompile/cross-compiler-m68k.tar.bz2
-echo "Copy cross-compiler-mips.tar.bz2 to /etc/xcompile"
-cp cross-compiler-mips.tar.bz2 /etc/xcompile/cross-compiler-mips.tar.bz2
-echo "Copy cross-compiler-mipsel.tar.bz2 to /etc/xcompile"
-cp cross-compiler-mipsel.tar.bz2 /etc/xcompile/cross-compiler-mipsel.tar.bz2
-echo "Copy cross-compiler-powerpc.tar.bz2 to /etc/xcompile"
-cp cross-compiler-powerpc.tar.bz2 /etc/xcompile/cross-compiler-powerpc.tar.bz2
-echo "Copy cross-compiler-sh4.tar.bz2 to /etc/xcompile"
-cp cross-compiler-sh4.tar.bz2 /etc/xcompile/cross-compiler-sh4.tar.bz2
-echo "Copy cross-compiler-sparc.tar.bz2 to /etc/xcompile"
-cp cross-compiler-sparc.tar.bz2 /etc/xcompile/cross-compiler-sparc.tar.bz2
-
 cd /etc/xcompile
+
+echo "Download cross-compiler-armv4l.tar.bz2 ..."
+wget https://www.uclibc.org/downloads/binaries/0.9.30.1/cross-compiler-armv4l.tar.bz2
+echo "Download cross-compiler-armv5l.tar.bz2 ..."
+wget https://www.uclibc.org/downloads/binaries/0.9.30.1/cross-compiler-armv5l.tar.bz2
+echo "Download cross-compiler-armv6l.tar.bz2 ..."
+wget http://distro.ibiblio.org/slitaz/sources/packages/c/cross-compiler-armv6l.tar.bz2
+echo "Download cross-compiler-i586.tar.bz2 ..."
+wget https://www.uclibc.org/downloads/binaries/0.9.30.1/cross-compiler-i586.tar.bz2
+echo "Download cross-compiler-m68k.tar.bz2 ..."
+wget https://www.uclibc.org/downloads/binaries/0.9.30.1/cross-compiler-m68k.tar.bz2
+echo "Download cross-compiler-mips.tar.bz2 ..."
+wget https://www.uclibc.org/downloads/binaries/0.9.30.1/cross-compiler-mips.tar.bz2
+echo "Download cross-compiler-mipsel.tar.bz2 ..."
+wget https://www.uclibc.org/downloads/binaries/0.9.30.1/cross-compiler-mipsel.tar.bz2
+echo "Download cross-compiler-powerpc.tar.bz2 ..."
+wget https://www.uclibc.org/downloads/binaries/0.9.30.1/cross-compiler-powerpc.tar.bz2
+echo "Download cross-compiler-sh4.tar.bz2 ..."
+wget https://www.uclibc.org/downloads/binaries/0.9.30.1/cross-compiler-sh4.tar.bz2
+echo "Download cross-compiler-sparc.tar.bz2 ..."
+wget https://www.uclibc.org/downloads/binaries/0.9.30.1/cross-compiler-sparc.tar.bz2
+
 echo "extracting cross-compiler-armv4l.tar.bz2 ..."
 tar -jxf cross-compiler-armv4l.tar.bz2
 echo "extracting cross-compiler-armv5l.tar.bz2 ..."
